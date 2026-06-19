267861eb3c10b8507d68223fb0de993ce303a8ab
Sam <samy.perissutti@gmail.com>
2026-06-20 00:31:36 +0200

    couleur changeant selon le temps de la progressBar

--- Scripts/jauge_menace.gd
+++ Scripts/jauge_menace.gd
@@ -1,7 +1,9 @@
 extends Control
 
 @export var value_max_jauge : float = 90
-
+var color1 = 1
+var color2 = 1
+var color3 = 1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,3 +13,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	$ProgressBar.value = Stats.time
 	$ProgressBar.max_value = value_max_jauge
+	color2 = 1 - Stats.time / value_max_jauge
+	color3 = 1 - Stats.time / value_max_jauge
+	$ProgressBar.modulate = Color(color1,color2,color3,1)
+	print(color2)
