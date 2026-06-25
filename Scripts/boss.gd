a98a9ee8a9cb11b6d667361bc39afc295233eb09
Sam <samy.perissutti@gmail.com>
2026-06-25 17:56:31 +0200

    Attaque fonctionnelle du boss, les deux mains attaquent, il manque juste à regler le timing de la collision par rappot au mains

--- Scripts/boss.gd
+++ Scripts/boss.gd
@@ -6,20 +6,23 @@ extends Node2D
 @export var vieBoss : float
 @export var vie_de_base = 100
 @export var facteur = 1
+var isAttacking: bool = false
+@onready var collideHandLeftAttack = $HandLeft/HandCollideLeft/CollisionShape2D
+@onready var collideHandRightAttack = $HandRight/HandCollideRight/CollisionShape2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	vieBoss = vie_de_base + (Stats.time * facteur)
+	collideHandLeftAttack.disabled = true
+	collideHandRightAttack.disabled = true
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if vieBoss <= 0:
 		spriteBody.play("death")
 		spriteHandLeft.play("hand_death")
 		spriteHandRight.play("hand_death")
-	else:
+	elif isAttacking == false:
 		spriteBody.play("idle")
 		spriteHandLeft.play("hand_idle")
 		spriteHandRight.play("hand_idle")
@@ -29,14 +32,47 @@ func takeDamageBoss(amount):
 	if vieBoss <= 0:
 		Damage.SendDied()
 		
-
-
-
-func _on_body_animation_finished() -> void:
-	if spriteBody.animation == "death":
-		queue_free()
-
-
 func _on_hand_collide_left_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		body.takeDamage(1)
+
+func _on_hand_collide_right_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		body.takeDamage(1)
+
+# --- Attaques Boss ---
+func attackBossHandLeft():
+	isAttacking = true
+	collideHandLeftAttack.disabled = false
+	spriteHandLeft.play("hand_attack")
+	print("Left")
+
+func attackBossHandRight():
+	isAttacking = true
+	collideHandRightAttack.disabled = false
+	spriteHandRight.play("hand_attack")
+	print("Right")
+
+# --- Animations finies ---
+func _on_body_animation_finished() -> void:
+	if spriteBody.animation == "death":
+		queue_free()
+		
+func _on_hand_left_animation_finished() -> void:
+	if spriteHandLeft.animation == "hand_attack":
+		isAttacking = false
+		collideHandLeftAttack.disabled = true
+		spriteHandLeft.animation = "hand_idle"
+
+func _on_hand_right_animation_finished() -> void:
+	if spriteHandRight.animation == "hand_attack":
+		isAttacking = false
+		collideHandRightAttack.disabled = true
+		spriteHandRight.animation = "hand_idle"
+
+# --- Timer finis ---
+func _on_timer_attack_hand_left_timeout() -> void:
+	attackBossHandLeft()
+
+func _on_timer_attack_hand_right_timeout() -> void:
+	attackBossHandRight()
